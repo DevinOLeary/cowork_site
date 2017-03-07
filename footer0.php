aa07db05d34e5d4a36d2c6151ba0101c1739887b
Devin O'Leary <djoleary07@gmail.com>
2017-03-07 13:16:22 -0500

    files restored and renamed

--- footer0.php
+++
@@ -1,8 +0,0 @@
-  <footer class="footer-block">
-
-  </footer>
-  <script src="<?php bloginfo('template_directory');?>/public/index.js"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAxnZHClVCmy5yXs9C0bBpWNh3KkpIpqQo&callback=initMap"
-   async defer></script>
-  </body>
-</html>
